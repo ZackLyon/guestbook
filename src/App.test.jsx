f705bf5b307bd9f12c7fc5f47fb9b0cf3c090371
Zack Lyon <zack.j.lyon@gmail.com>
2021-12-14 18:17:04 -0800

    Add passing test

---
+++ src/App.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+it('should pass a test', () => {
+  expect(true).toEqual(true);
+});
